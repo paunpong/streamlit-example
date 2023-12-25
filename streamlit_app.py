e944b0cca72026b3ade5fc3d51dc52cba1969107
paunpong <154779459+paunpong@users.noreply.github.com>
2023-12-25 15:25:23 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,20 @@
+!wget https://github.com/Phonbopit/sarabun-webfont/raw/master/fonts/thsarabunnew-webfont.ttf
+# !pip install -U --pre matplotlib
+import matplotlib as mpl
+mpl.font_manager.fontManager.addfont('thsarabunnew-webfont.ttf') # 3.2+
+mpl.rc('font', family='TH Sarabun New')
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import io
+import statistics as st
+import re
+import operator
 import altair as alt
 import numpy as np
 import pandas as pd
 import streamlit as st
 
 """
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
+โปรแกรมสร้างรายงานสรุปผลจากฟอร์มออนไลน์
 """
-
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
-
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
-
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
-
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
