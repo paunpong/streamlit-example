63b86a3f416694d015785cdfda6a5f9a3c25a455
paunpong <154779459+paunpong@users.noreply.github.com>
2023-12-26 23:18:42 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ st.title('กรุณาใส่ไฟล์ที่เป็น excel')
 
 upload_file = st.file_uploader("Upload File",type=["csv", "xlsx"])
 
-df.fillna('ไม่ระบุ',inplace=True)
-df.replace('-','ไม่ระบุ',inplace=True)
+st.fillna('ไม่ระบุ',inplace=True)
+st.replace('-','ไม่ระบุ',inplace=True)
 
 
