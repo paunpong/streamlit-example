989ea3522ca91280410b83f94fcfc86201606f44
paunpong <154779459+paunpong@users.noreply.github.com>
2023-12-26 11:39:33 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,16 @@
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-import io
-import statistics as st
-import re
-import operator
-import altair as alt
+#import matplotlib as mpl
+#import matplotlib.pyplot as plt
+#import numpy as np
+#import pandas as pd
+#import io
+#import statistics as st
+#import re
+#import operator
+#import altair as alt
 import numpy as np
 import pandas as pd
 import streamlit as st
 
-"""
-โปรแกรมสร้างรายงานสรุปผลจากฟอร์มออนไลน์
-"""
+
+st.head('โปรแกรมสร้างรายงานสรุปผลจากฟอร์มออนไลน์')
+
