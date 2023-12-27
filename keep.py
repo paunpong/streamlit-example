87682312176b656567ffebe755c2135748c55495
paunpong <154779459+paunpong@users.noreply.github.com>
2023-12-27 17:33:25 +0700

    Update keep.py

--- keep.py
+++ keep.py
@@ -9,6 +9,8 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
+digit = int(2)
+
 def upload(A):
   if upload_file is not None:
     y = upload_file.name.split(".")[1].lower()
@@ -20,6 +22,17 @@ def upload(A):
     df.fillna('ไม่ระบุ',inplace=True)
     df.replace('-','ไม่ระบุ',inplace=True)
     st.dataframe(df)
+
+def count_list(A,removenan=True):
+  if removenan and 'ไม่ระบุ'in A:
+    A = [n for n in A if n != 'ไม่ระบุ']
+  list_A = list(set(A))
+  count_dict = dict()
+  len_A = len(A)
+  for c in list_A:
+    per = (A.count(c)/len_A)*100
+    count_dict[c] = {"count":A.count(c),"percent":round(per,digit)}
+  return count_dict    
 
 st.header('โปรแกรมสร้างรายงานสรุปผลจากฟอร์มออนไลน์')
 st.title('กรุณาใส่ไฟล์ที่เป็น excel')
