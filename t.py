9478a727cd02a9a26f1b113eed630fa2596e29a0
paunpong <154779459+paunpong@users.noreply.github.com>
2023-12-27 19:00:23 +0700

    Update t.py

--- t.py
+++ t.py
@@ -1,28 +1,20 @@
+font_properties = {'family': 'TH Sarabun New', 'weight': 'normal', 'size': 12}
+import matplotlib.font_manager as fm
+
+font_path = fm.findfont(FontProperties(family='TH Sarabun New'))
+print(f"Found font at: {font_path}")
 import streamlit as st
 import matplotlib.pyplot as plt
-from matplotlib.font_manager import FontProperties
-
-# Specify the correct path to the "Noto Sans Thai" font file
-font_path = "/path/to/'thsarabunnew-webfont.ttf'"
-
-# Specify the correct font properties
-font_properties = FontProperties(fname=font_path, size=12)
-
-# Your Streamlit app code goes here...
 
 # Example data
 labels = ['ก', 'ข', 'ค', 'ง', 'จ']
 values = [5, 8, 3, 7, 2]
 
-# Create a bar plot
+# Create a bar plot without specifying font
 plt.bar(labels, values)
-
-# Set Thai font for the plot
-plt.xlabel('แกน X', fontproperties=font_properties)
-plt.ylabel('แกน Y', fontproperties=font_properties)
-plt.title('กราฟแท่ง', fontproperties=font_properties)
 
 # Display the plot in Streamlit
 st.pyplot()
+
 
 
