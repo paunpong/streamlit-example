e13c3af0281b89d86d0a875c7afeda101620fd72
paunpong <154779459+paunpong@users.noreply.github.com>
2023-12-27 17:15:11 +0700

    Update keep.py

--- keep.py
+++ keep.py
@@ -14,9 +14,10 @@ st.title('กรุณาใส่ไฟล์ที่เป็น excel')
 upload_file = st.file_uploader("Upload File",type=["csv", "xlsx"])
 
 if upload_file is not None:
-  if '.xlsx' in upload_file:
+  x = upload_file.split('.')[1]
+  if 'xlsx' in x:
     df = pd.read_excel(upload_file)
-  else:
+  elif 'csv' in x:
     df = pd.read_csv(upload_file)
 
   df.fillna('ไม่ระบุ',inplace=True)
