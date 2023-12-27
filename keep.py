1a2af2a269ae03279184e22c6ba5c1934f911d77
paunpong <154779459+paunpong@users.noreply.github.com>
2023-12-27 18:54:12 +0700

    Update keep.py

--- keep.py
+++ keep.py
@@ -59,18 +59,19 @@ upload_file = st.file_uploader("Upload File",type=["csv", "xlsx"])
 
 upload_df = upload(upload_file)
 
-list_question = [h for h in upload_df]
+def question(A):
+  list_question = [h for h in upload_df]
 
-if ('Times' or 'ประทับเวลา') in list_question[0]:
-  list_question.pop(0)
+  if ('Times' or 'ประทับเวลา') in list_question[0]:
+    list_question.pop(0)
 
-for key in list_question:
-  column = upload_df[key].values.tolist()
-  len_column = len(column)
+  for key in list_question:
+    column = upload_df[key].values.tolist()
+    len_column = len(column)
   
-  if '*' in key:
-    list_pie_chart[key]=True
-    continue
+    if '*' in key:
+      list_pie_chart[key]=True
+      continue
 
-for p in list_pie_chart:
-  pie_chart(count_list(upload_df[p].values.tolist()),p)
+  for p in list_pie_chart:
+    pie_chart(count_list(upload_df[p].values.tolist()),p)
