a035dc22f7da41bbddb0b8d9a71bad1159aa9763
paunpong <154779459+paunpong@users.noreply.github.com>
2023-12-28 06:05:59 +0700

    Update keep.py

--- keep.py
+++ keep.py
@@ -69,7 +69,7 @@ if ('Times' or 'ประทับเวลา') in list_question[0]:
   list_question.pop(0)
   
 for key in list_question:
-  column = list_query[key].values.tolist()
+  column = list_question[key].values.tolist()
   len_column = len(column)
   
   if '*' in key:
