3037ecfbe0a5e65c59021d87596ec395ebc3f1c6
paunpong <154779459+paunpong@users.noreply.github.com>
2023-12-27 13:27:56 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ if upload_file is not None :
     #df.fillna('ไม่ระบุ',inplace=True)
     #df.replace('-','ไม่ระบุ',inplace=True)
     'pandas done'
-y = list(upload)[0]    
-df = pd.read_excel(io.BytesIO(upload[y]))
+y = list(upload_file)[0]    
+df = pd.read_excel(io.BytesIO(upload_file[y]))
 st.write(df)
 
