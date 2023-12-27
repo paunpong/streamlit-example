291f2728d94f4042fb094d1385c1d5ea1a4a2f63
paunpong <154779459+paunpong@users.noreply.github.com>
2023-12-27 18:45:14 +0700

    Update keep.py

--- keep.py
+++ keep.py
@@ -1,4 +1,7 @@
+!wget https://github.com/Phonbopit/sarabun-webfont/raw/master/fonts/thsarabunnew-webfont.ttf
 import matplotlib as mpl
+mpl.font_manager.fontManager.addfont('thsarabunnew-webfont.ttf') # 3.2+
+mpl.rc('font', family='TH Sarabun New')
 import matplotlib.pyplot as plt
 import statistics as stat
 #import io
