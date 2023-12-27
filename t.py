fc8f2eec658c8d56135c91f065ee3a624fd81f3c
paunpong <154779459+paunpong@users.noreply.github.com>
2023-12-27 19:02:07 +0700

    Update t.py

--- t.py
+++ t.py
@@ -1,20 +1,29 @@
-font_properties = {'family': 'TH Sarabun New', 'weight': 'normal', 'size': 12}
-import matplotlib.font_manager as fm
-
-font_path = fm.findfont(FontProperties(family='TH Sarabun New'))
-print(f"Found font at: {font_path}")
 import streamlit as st
 import matplotlib.pyplot as plt
+from matplotlib.font_manager import FontProperties
+
+# Specify the font properties
+font_properties = {'family': 'TH Sarabun New', 'weight': 'normal', 'size': 12}
 
 # Example data
 labels = ['ก', 'ข', 'ค', 'ง', 'จ']
 values = [5, 8, 3, 7, 2]
 
-# Create a bar plot without specifying font
-plt.bar(labels, values)
+# Create a bar plot with specified font
+fig, ax = plt.subplots()
+ax.bar(labels, values)
+
+# Set font properties for the plot
+for label in ax.get_xticklabels() + ax.get_yticklabels():
+    label.set_fontproperties(FontProperties(**font_properties))
+
+ax.set_xlabel('แกน X', **font_properties)
+ax.set_ylabel('แกน Y', **font_properties)
+ax.set_title('กราฟแท่ง', **font_properties)
 
 # Display the plot in Streamlit
-st.pyplot()
+st.pyplot(fig)
+
 
 
 
