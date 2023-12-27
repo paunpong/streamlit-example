bc2c118c94c94114e8b769f1a0be03028f1e0f82
paunpong <154779459+paunpong@users.noreply.github.com>
2023-12-27 19:04:58 +0700

    Update t.py

--- t.py
+++ t.py
@@ -2,14 +2,14 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import matplotlib.font_manager as fm
 
-# Find the font file for 'TH Sarabun New'
-font_path = fm.findfont(fm.FontProperties(family='TH Sarabun New'))
+# Find the font file for 'Noto Sans Thai'
+font_path = fm.findfont(fm.FontProperties(family='Noto Sans Thai'))
 
 # Check if the font was found
 if font_path:
     st.success(f"Found font at: {font_path}")
 else:
-    st.error("Font not found. Make sure 'TH Sarabun New' is installed on your system.")
+    st.error("Font not found. Make sure 'Noto Sans Thai' is installed on your system.")
 
 # Example data
 labels = ['ก', 'ข', 'ค', 'ง', 'จ']
