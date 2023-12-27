c8aefe7b1375e4e809ed2ffc3735b98516d233ad
paunpong <154779459+paunpong@users.noreply.github.com>
2023-12-27 16:50:10 +0700

    Update keep.py

--- keep.py
+++ keep.py
@@ -1,1 +1,16 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import io
+import statistics as stat
+import re
+import operator
+import altair as alt
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+st.header('โปรแกรมสร้างรายงานสรุปผลจากฟอร์มออนไลน์')
+st.title('กรุณาใส่ไฟล์ที่เป็น excel')
+upload_file = st.file_uploader("Upload File",type=["csv", "xlsx"])
+
 
