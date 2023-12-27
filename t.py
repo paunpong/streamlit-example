bcb6665df74657ee6ad1e0a36cf97f25e69f6e1c
paunpong <154779459+paunpong@users.noreply.github.com>
2023-12-27 19:27:52 +0700

    Update t.py

--- t.py
+++ t.py
@@ -1,28 +1,20 @@
+# download font file มาก่อน
+!wget https://awards.opdc.go.th/awards_opdc/assets/fonts/THSarabunNew/THSarabunNew.ttf
+
+# import and install
 import streamlit as st
 import matplotlib.pyplot as plt
-from matplotlib.font_manager import FontProperties
+import matplotlib.font_manager as fm
 
-# Specify the Thai font properties
-font_properties = {'family': 'TH Sarabun New', 'weight': 'normal', 'size': 12}
+font_list = fm.createFontList(['THSarabunNew.ttf'])
+fm.fontManager.ttflist.extend(font_list)
 
-# Example data
-labels = ['ก', 'ข', 'ค', 'ง', 'จ']
-values = [5, 8, 3, 7, 2]
-
-# Create a bar plot with specified font
-fig, ax = plt.subplots()
-ax.bar(labels, values)
-
-# Set font properties for the plot
-for label in ax.get_xticklabels() + ax.get_yticklabels():
-    label.set_fontproperties(FontProperties(**font_properties))
-
-ax.set_xlabel('แกน X', **font_properties)
-ax.set_ylabel('แกน Y', **font_properties)
-ax.set_title('กราฟแท่ง', **font_properties)
-
-# Display the plot in Streamlit
-st.pyplot(fig)
+# set font
+plt.rcParams['font.family'] = 'TH Sarabun New'
+plt.rcParams['xtick.labelsize'] = 20.0
+plt.rcParams['ytick.labelsize'] = 20.0
+# plot
+st.bar(['ก','ข','ค'], [1,2,3]);
 
 
 
