c927f79db0c38983ab5160c50faddf6f0dfd327e
paunpong <154779459+paunpong@users.noreply.github.com>
2024-01-03 22:37:28 +0700

    Update keep.py

--- keep.py
+++ keep.py
@@ -223,7 +223,7 @@ for p in list_pie_chart:
   percent = values[k]['percent']
   table_data.append([k, count, percent])
   #st.write(k , '\t' , count , '\t' , percent)
-if table_data != 0:
+if table_data is not None:
  st.table([table_head, *table_data]) 
 for p in list_pie_chart:
  pie_chart(count_list(upload_df[p].values.tolist()),p)
