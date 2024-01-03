994b968c54ffe6289a93d2f2ba0388d7c2ccaf57
paunpong <154779459+paunpong@users.noreply.github.com>
2024-01-03 18:09:02 +0700

    Update keep.py

--- keep.py
+++ keep.py
@@ -216,14 +216,14 @@ if upload_file is not None:
 if upload_file is not None:
  table_head = ['หัวข้อ' , 'จำนวน' , 'เปอร์เซ็นต์']
  table_data = []
- for p in list_pie_chart:
-  values = count_list(upload_df[p].values.tolist(), list_pie_chart[p])
-  for k in values:
-   count = values[k]['count']
-   percent = values[k]['percent']
-   table_data.append([k, count, percent])
+for p in list_pie_chart:
+ values = count_list(upload_df[p].values.tolist(), list_pie_chart[p])
+ for k in values:
+  count = values[k]['count']
+  percent = values[k]['percent']
+  table_data.append([k, count, percent])
    #st.write(k , '\t' , count , '\t' , percent)
-  st.table([table_head, *table_data])
+ st.table([table_head, *table_data])
  for p in list_pie_chart:
   pie_chart(count_list(upload_df[p].values.tolist()),p)
 
