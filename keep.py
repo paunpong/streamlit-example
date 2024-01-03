7444a891e23682115c0b19efe261709a17ce95e3
paunpong <154779459+paunpong@users.noreply.github.com>
2024-01-03 12:56:19 +0700

    Update keep.py

--- keep.py
+++ keep.py
@@ -100,7 +100,10 @@ def pie_chart(data,key):
   counts = [data[key]['percent']for key in data]
   #x,ax = plt.subplots()
   plt.pie(counts, labels=labels, autopct=f'%.{digit}f')
-  add_font_thai()
+  for item in ([ax.title, ax.xaxis.label, ax.yaxis.label] + ax.get_xticklabels() + ax.get_yticklabels()):
+   item.set_fontproperties(thai_font_prop)
+ 
+  ax.legend(prop=thai_font_prop)
   plt.title(key)
   st.pyplot()
   #plt.show()
