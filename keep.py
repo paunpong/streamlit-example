6da18d3f61e5a3c36120b383b4eb09677e5dd4b5
paunpong <154779459+paunpong@users.noreply.github.com>
2024-01-04 19:55:31 +0700

    Update keep.py

--- keep.py
+++ keep.py
@@ -93,7 +93,7 @@ def pie_chart(data, key):
  labels = [str(key) for key in data]
  counts = [data[key]['percent'] for key in data]
  fig,ax = plt.subplots()
- wedges, texts, autotexts = ax.pie(counts, labels, autopct=f'%.{digit}f', textprops={'fontproperties': thai_font_prop})
+ wedges, texts, autotexts = ax.pie(counts, labels=labels, autopct=f'%.{digit}f', textprops={'fontproperties': thai_font_prop})
  for text in texts + autotexts:
   text.set_fontproperties(thai_font_prop)
  plt.title(key, fontproperties=thai_font_prop)
@@ -151,7 +151,7 @@ def bar_chart(data,key):
    values = [data.count(i) for i in set(data) if i != 'ไม่ระบุ']
    labels = [str(i) for i in set(data) if i != 'ไม่ระบุ']
  fig,ax = plt.subplots(figsize=(9,6))
- wedges, texts, autotexts = ax.bar(values, labels=labels, autopct=f'%.{digit}f', textprops={'fontproperties': thai_font_prop})
+ wedges, texts, autotexts = ax.bar(values, labels,  textprops={'fontproperties': thai_font_prop})
  for text in texts + autotexts:
   text.set_fontproperties(thai_font_prop)
  ax.set_title(key, fontproperties=thai_font_prop)
