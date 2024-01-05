fd5ad137ba45e93e1421a2228f091f795f898285
paunpong <154779459+paunpong@users.noreply.github.com>
2024-01-05 11:51:59 +0700

    Update keep.py

--- keep.py
+++ keep.py
@@ -153,9 +153,9 @@ def bar_chart(data,key):
 def stacked_bar(data,key):
  fig,ax = plt.subplots()
  name = data.keys()
- ax.set_yticklabels(index=name, fontproperties=thai_font_prop)
  d_f = pd.DataFrame(data.values(),index=name)
  d_f.plot.barh(stacked=True, figsize=(9,4)).legend(loc='upper right');
+ ax.set_yticklabels(name, fontproperties=thai_font_prop)
  plt.title(key,fontproperties=thai_font_prop)
  st.pyplot()
 
