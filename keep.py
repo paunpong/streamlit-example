5b979d75783aba77e6eb01a9cf625f1a1b5c1e58
paunpong <154779459+paunpong@users.noreply.github.com>
2024-01-05 14:10:43 +0700

    Update keep.py

--- keep.py
+++ keep.py
@@ -367,3 +367,12 @@ if option_3:
 
 if option_4:
    st.write("Hello world 4")
+
+b = (
+    Bar()
+    .add_xaxis(["Microsoft", "Amazon", "IBM", "Oracle", "Google", "Alibaba"])
+    .add_yaxis("2017-2018 Revenue in (billion $)", random.sample(range(100), 10))
+    .set_global_opts(
+        title_opts=opts.TitleOpts(
+            title="Top cloud providers 2018", subtitle="2017-2018 Revenue"
+        )
