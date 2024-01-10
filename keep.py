216991d4d38cb14381cc7f06138f42a1544d3a27
paunpong <154779459+paunpong@users.noreply.github.com>
2024-01-10 20:26:16 +0700

    Update keep.py

--- keep.py
+++ keep.py
@@ -247,8 +247,7 @@ if upload_file is not None:
  #for topic in Dic_type_chart:
   if Dic_type_chart[topic] == 'bar_chart':
    list_bar_chart[topic]={'removenan':True,'orther_number':1}
-   if to in list_bar_chart[topic]:
-    del list_pie_chart[to]
+   list_pie_chart.remove[topic]
   x = st.sidebar.radio(topic, ["Reomve_nan", "add_nan"], horizontal=True ,index=0)
   if x =="Reomve_nan":
    list_pie_chart[topic]=True
