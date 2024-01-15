a0c4390d02482b23f5e3aea50fb85fe259aacb98
paunpong <154779459+paunpong@users.noreply.github.com>
2024-01-15 13:39:10 +0700

    Update keep.py

--- keep.py
+++ keep.py
@@ -280,8 +280,8 @@ if menu == 'เริ่มต้นโปรแกรม':
      list_pie_chart[topic]={'removenan':True}
      if 'orther_number' in list_bar_chart[topic]:
       del list_bar_chart[topic]
-   st.write(list_bar_chart) 
    for topic in list_str_keys:
+    st.write(topic)
     str = st.radio(topic,['แผนภูมิแท่งแบบต่อกัน','แผ่นภูมิแท่ง'], horizontal=True)
     if str == 'แผนภูมิแท่ง':
      list_bar_chart[topic]={'removenan':True,'orther_number':1}
