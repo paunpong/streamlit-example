6fc2f3d558cffd9b3ea4748f42ed93ce4c16947e
paunpong <154779459+paunpong@users.noreply.github.com>
2024-01-16 15:24:07 +0700

    Update keep.py

--- keep.py
+++ keep.py
@@ -271,7 +271,7 @@ if menu == 'เริ่มต้นโปรแกรม':
      endtext = "ฯ"
    for topic in list_pie_keys:
     numberitem = 0
-    head_bulet = stt(list(numberitem)) + ". "+topic[:x]+endtext
+    head_bulet = str(list(numberitem)) + ". "+topic[:x]+endtext
     p = st.radio(head_bulet, ['แผนภูมิวงกลม', 'แผนภูมิแท่ง'], horizontal=True)
     st.text("")
     if p == 'แผนภูมิแท่ง':
