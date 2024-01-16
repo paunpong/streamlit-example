dbd1d085bee870b5f5b66efa87b79c76687f283f
paunpong <154779459+paunpong@users.noreply.github.com>
2024-01-16 14:17:40 +0700

    Update keep.py

--- keep.py
+++ keep.py
@@ -263,8 +263,9 @@ if menu == 'เริ่มต้นโปรแกรม':
   
   tab1, tab2 = st.sidebar.tabs(['ประเภทแผนภูมิ', 'ปรับแต่งรายระเอียดแผนภูมิ'])
   with tab1:
-   number_item = 0
+   
    for topic in list_pie_keys:
+    number_item = 0
     head_bulet = str(number_item + 1)+". "+topic
     p = st.radio(head_bulet, ['แผนภูมิวงกลม', 'แผนภูมิแท่ง'], horizontal=True)
     if p == 'แผนภูมิแท่ง':
