2d1d96ec95f2a3e21b9c0bcce8d6e6f222b336a6
paunpong <154779459+paunpong@users.noreply.github.com>
2024-01-17 20:35:20 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -67,7 +67,7 @@ if upload_file is not None:
     col = []
     for n in list_stackbar:
       if i in n:
-        col = col + col(n)
+        col = col + col.append(n)
     st.write(col)
     #if num_check(Column) and set(Column).issubset({1,2,3,4,5,'ไม่ระบุ'}):
       #st.write('11')
