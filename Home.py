50c66bc60985b820680da3b9bc7da30454452164
paunpong <154779459+paunpong@users.noreply.github.com>
2024-01-17 15:48:52 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,5 +1,7 @@
 import streamlit as st
-st.set_page_config(page_title="Home",initial_sidebar_state="expanded",layout="wide")
+import numpy as np
+import pandas as pd
+#st.set_page_config(page_title="Home",initial_sidebar_state="expanded",layout="wide")
 st.write('สวัสดี')
 
 def upload(A):
