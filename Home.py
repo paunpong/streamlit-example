d5ef8f08314c8e2b7ec08cd617ec0020dde18474
paunpong <154779459+paunpong@users.noreply.github.com>
2024-01-17 20:17:11 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -56,7 +56,7 @@ if upload_file is not None:
     list_question.pop(0)
   for key in list_question:
     column = upload_df[key].values.tolist()
-    st.write(column)
+    st.write(key,column)
     len_column = len(column)
     x = Count(column)
     if '[' in key:
@@ -64,12 +64,13 @@ if upload_file is not None:
       topic = Split(list_stackbar)
       list_topic_stackbar.append(topic)
     set_topic = set(list_topic_stackbar)
-  for i in set_topic:
-    col = []
-    for n in list_stackbar:
-      if i in n:
-        col.append(n)
-    Col = upload_df[col].values.tolist()
+    for i in set_topic:
+      col = []
+      for n in list_stackbar:
+        if i in n:
+          col.append(n)
+      Col = upload_df[col].values.tolist()
+      st.write(list_stackbar,Col)
     #if num_check(Column) and set(Column).issubset({1,2,3,4,5,'ไม่ระบุ'}):
       #st.write('11')
       #for key in list_stackbar:
