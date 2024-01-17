9a9436021cd80c0747a9add35a359e2bed0058f2
paunpong <154779459+paunpong@users.noreply.github.com>
2024-01-17 23:40:05 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -75,7 +75,8 @@ if upload_file is not None:
         col.append(n)
     colu = col
     st.write(colu)
-      #Column = upload_df[col].values.tolist()    
+    Column = upload_df[colu].values.tolist()
+    st.write(Column)
       #sum_Column = sum(Column,[])
       #if num_check(sum_Column)and set(sum_Column).issubset({1,2,3,4,5,'ไม่ระบุ'}):
         #for key in list_stackbar:
