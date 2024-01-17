c52fb126d2cc0a7a0095f874915e0c9be981c87c
paunpong <154779459+paunpong@users.noreply.github.com>
2024-01-17 17:39:31 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -62,8 +62,8 @@ if upload_file is not None:
       list_stackbar.append(key)
       topic = Split(list_stackbar)
       list_topic_stackbar.append(topic)
-  st.write(list_topic_stackbar)
-  st.write(set(list_stackbar))
+  st.write(set(list_topic_stackbar))
+  st.write(list_stackbar)
     
     
             
