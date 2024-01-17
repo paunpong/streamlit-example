1c3a25f8ecf1a62fdb6c3d455225bd746606302d
paunpong <154779459+paunpong@users.noreply.github.com>
2024-01-17 20:01:44 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -69,9 +69,8 @@ if upload_file is not None:
       if i in n:
         col.append(n)
     Column = upload_df[col].values.tolist()
-    st.write(i,Column)
-    #if num_check(upload_df[col].values.tolist()) and set(upload_df[col]).issubset({1,2,3,4,5,'ไม่ระบุ'}):
-      #st.write('11')
+    if num_check(Column) and set(Column).issubset({1,2,3,4,5,'ไม่ระบุ'}):
+      st.write('11')
       #for key in list_stackbar:
         #st.write(key)
         #for topic in key:
