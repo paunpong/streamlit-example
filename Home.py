fdbb4ab21464ffe3821216eaf9c2c96ed20a91ed
paunpong <154779459+paunpong@users.noreply.github.com>
2024-01-17 17:38:44 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -64,10 +64,7 @@ if upload_file is not None:
       list_topic_stackbar.append(topic)
   st.write(list_topic_stackbar)
   st.write(set(list_stackbar))
-    if list_topic_stackbar in list_stackbar:
-      st.write(True)
-    if list_topic_stackbar not in list_stackbar:
-      st.write(False)
+    
     
             
     
