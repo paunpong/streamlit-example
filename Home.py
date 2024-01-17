c23299cd58b96124b6f7ba23470a11743b177a0b
paunpong <154779459+paunpong@users.noreply.github.com>
2024-01-17 23:45:39 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -79,6 +79,6 @@ if upload_file is not None:
     sum_Column = sum(Column,[])
     st.write(sum_Column)
     if num_check(sum_Column)and set(sum_Column).issubset({1,2,3,4,5,'ไม่ระบุ'}):
-      for i in renge(5):
+      for i in range(5):
         print(i)
       
