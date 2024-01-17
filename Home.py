a4b88cb6e474b7625d5ee29d1193fe3e2def3bf2
paunpong <154779459+paunpong@users.noreply.github.com>
2024-01-17 15:36:55 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,3 +1,18 @@
 import streamlit as st
 st.set_page_config(page_title="Home",initial_sidebar_state="expanded",layout="wide")
-st.write('สวัสดี') 
+st.write('สวัสดี')
+
+def upload(A):
+  if A is not None:
+    y = A.name.split(".")[1]
+  if 'xlsx' in y:
+    df = pd.read_excel(A)
+  elif 'csv' in y:
+    df = pd.read_csv(A)
+  df.fillna('ไม่ระบุ',inplace=True)
+  df.replace('-','ไม่ระบุ',inplace=True)
+  df.replace(' ','ไม่ระบุ',inplace=True)
+   #st.dataframe(df)  
+  return df
+
+
