7b2c27f627ce4bbbcf7cde40707547ce7cf4fb6f
paunpong <154779459+paunpong@users.noreply.github.com>
2024-01-18 01:19:19 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -37,11 +37,11 @@ def stacked_bar(data,key):
  fig,ax = plt.subplots()
  name = data.keys()
  data = data.values()
- ax.set_yticklabels(name, fontproperties=thai_font_prop)
+ ax.set_yticklabels(name)
  d_f = pd.DataFrame(data,index=name)
- d_f.plot.barh(stacked=True, figsize=(9,4),ax=ax).legend(bbox_to_anchor=(1, 0, 0.16, 1),prop=thai_font_prop)
- #stacked_plot.legend(loc='upper right', fontproperties=thai_font_prop)
- plt.title(key,fontproperties=thai_font_prop)
+ d_f.plot.barh(stacked=True, figsize=(9,4),ax=ax).legend(bbox_to_anchor=(1, 0, 0.16, 1))
+ #stacked_plot.legend(loc='upper right')
+ plt.title(key)
  st.pyplot()
 
 def stat(A):
