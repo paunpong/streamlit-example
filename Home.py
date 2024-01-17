215f89a0de3a3bee951a2bdef2637db235c1be50
paunpong <154779459+paunpong@users.noreply.github.com>
2024-01-17 23:01:30 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -75,7 +75,8 @@ if upload_file is not None:
     Column = upload_df[col].values.tolist()    
     sum_Column = sum(Column,[])
     st.write(set(sum_Column))
-    if num_check(sum_Column) and set(sum_Column).issubset({1,2,3,4,5,'ไม่ระบุ'}):
+    if num_check(sum_Column):
+      st.write(sum_Column)
       for key in list_stackbar:
         for topic in key:
           list_stack_num[key]=True
@@ -87,5 +88,5 @@ if upload_file is not None:
 
             
     
-      
+'''and set(sum_Column).issubset({1,2,3,4,5,'ไม่ระบุ'}'''      
       
