4b860799a6c535abc5704f10987dd34813a059dd
paunpong <154779459+paunpong@users.noreply.github.com>
2024-01-17 16:43:03 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -29,7 +29,6 @@ def Count(A,removenan=True):
   if removenan and 'ไม่ระบุ' in A:
     A = [n for n in A if n != 'ไม่ระบุ']
   list_A = list(set(A))
-  st.write(list_A)
   counta = dict()
   for i in list_A:
     counta[i] = A.count(i)
@@ -37,7 +36,7 @@ def Count(A,removenan=True):
 
 def Split(A):
   for i in A:
-    topic_word = i.split(' [')[:1]
+    topic_word = i.split('[')[:1]
     topic = set(topic_word)
     #topic_word = topic_word.strip() 
   return topic  
@@ -47,6 +46,8 @@ upload_file = st.sidebar.file_uploader(" ",type=["csv", "xlsx"])
 upload_df = upload(upload_file)
 
 if upload_file is not None:
+  list_topic_stackbar=[]
+  list_stackbar=[]
   list_question = [h for h in upload_df]
   if ('Times' or 'ประทับเวลา') in list_question[0]:
     list_question.pop(0)
@@ -54,13 +55,10 @@ if upload_file is not None:
     column = upload_df[key].values.tolist()
     len_column = len(column)
     x = Count(column)
-    list_topic_stackbar=[]
-    list_stackbar=[]
     if '[' in key:
       list_stackbar.append(key)
       topic = Split(list_stackbar)
-      list_topic_stackbar.append(topic)
-    st.write(list_topic_stackbar)      
+      list_topic_stackbar.append(topic)     
     
       
       
