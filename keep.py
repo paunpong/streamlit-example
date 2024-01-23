f424328306f482d070fef3d11b5898de1e8fd4d6
paunpong <154779459+paunpong@users.noreply.github.com>
2024-01-23 16:29:50 +0700

    Update keep.py

--- keep.py
+++ keep.py
@@ -421,7 +421,8 @@ if menu == 'เริ่มต้นโปรแกรม':
     for i in dict_num_stack:
       stacked_bar(dict_num_stack[i],i)
     for i in list_stack_bar:
-     a = change_num_to_text(i)
+     col = upload_df[i].values.tolist()
+     a = change_num_to_text(col)
      c = count_list(a)
      for k in c:
       c[k] = c[k]['percent']
