1166b3e0ee2adbbcc36455f347d18ebc747df97f
paunpong <154779459+paunpong@users.noreply.github.com>
2024-01-23 21:52:02 +0700

    Update keep.py

--- keep.py
+++ keep.py
@@ -194,12 +194,14 @@ def bar_chart1(data,key,orther_number=1):
  st.pyplot()
 
 def bar_chart_new(data,key):
- labels = range(1,len(data[0])+1)
+ rang = range(1,len(data[0]))
+ labels = rang + 1
+ label = data[0]
  values = data[1]
  fig,ax = plt.subplots(figsize=(9,6))
  ax.set_xticklabels(labels, fontproperties=thai_font_prop)
  ax.bar(labels, values)
- ax.legend(labels = data[0],bbox_to_anchor=(1, 0, 0.16, 1), prop=thai_font_prop)
+ ax.legend(labels = label,bbox_to_anchor=(1, 0, 0.16, 1), prop=thai_font_prop)
  plt.title(key, fontproperties=thai_font_prop)
  st.pyplot()
 
