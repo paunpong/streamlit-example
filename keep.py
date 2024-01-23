35eb559a19096dee814d3cf394e0f89d632949a9
paunpong <154779459+paunpong@users.noreply.github.com>
2024-01-23 21:19:59 +0700

    Update keep.py

--- keep.py
+++ keep.py
@@ -194,7 +194,7 @@ def bar_chart1(data,key,orther_number=1):
  st.pyplot()
 
 def bar_chart_new(data,key):
- labels = data[0]
+ labels = len(data[0])
  st.write(labels,'la')
  values = data[1]
  st.write(len(data[0]))
