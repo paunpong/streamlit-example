63e7819162c671768d8db52754abc7eaef260d6f
paunpong <154779459+paunpong@users.noreply.github.com>
2024-01-23 22:25:09 +0700

    Update keep.py

--- keep.py
+++ keep.py
@@ -201,7 +201,7 @@ def bar_chart_new(data,key):
  ax.set_xticklabels(labels, fontproperties=thai_font_prop)
  ax.set_xticks(labels)
  ax.bar(labels, values)
- ax.legend(labels =[f'{labels[i]}:{label[x]}' for i in labels and for x in label ],bbox_to_anchor=(1, 0, 0.22, 1), prop=thai_font_prop)
+ ax.legend(labels =[f'{labels[i]}:{label[i]}' for i in labels],bbox_to_anchor=(1, 0, 0.22, 1), prop=thai_font_prop)
  plt.title(key, fontproperties=thai_font_prop)
  st.pyplot()
 
