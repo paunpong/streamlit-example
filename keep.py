2553a4a8fcf6f57cc13416711a982257467b79d7
paunpong <154779459+paunpong@users.noreply.github.com>
2024-01-24 20:21:07 +0700

    Update keep.py

--- keep.py
+++ keep.py
@@ -352,17 +352,14 @@ if menu == 'เริ่มต้นโปรแกรม':
    if list_bar_keys != list():
     st.markdown("""---""")
    for topic in list_str_keys:
+    numberitem = numberitem+1
     str = st.radio(topic[:x]+endtext,['แผนภูมิแท่งแบบต่อกัน'], horizontal=True)
    for topic in list_num_keys:
     numberitem = numberitem+1
-    strnumberitem = str(numberitem)+')'
-    head_bulet = strnumberitem + topic[:x]+endtext
-    num = st.radio(head_bulet,['แผนภูมิแท่งแบบต่อกัน'], horizontal=True)
+    num = st.radio(topic[:x]+endtext,['แผนภูมิแท่งแบบต่อกัน'], horizontal=True)
    for topic in list_stack_keys:
     numberitem = numberitem+1
-    strnumberitem = str(numberitem)+')'
-    head_bulet = strnumberitem + topic[:x]+endtext
-    stack = st.radio(head_bulet,['แผนภูมิแท่งแบบต่อกัน'], horizontal=True)
+    stack = st.radio(topic[:x]+endtext,['แผนภูมิแท่งแบบต่อกัน'], horizontal=True)
     
     
      
