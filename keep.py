273d9bfa59bed27e92093499c39091e53b06a087
paunpong <154779459+paunpong@users.noreply.github.com>
2024-01-24 20:08:36 +0700

    Update keep.py

--- keep.py
+++ keep.py
@@ -352,9 +352,6 @@ if menu == 'เริ่มต้นโปรแกรม':
    if list_bar_keys != list():
     st.markdown("""---""")
    for topic in list_str_keys:
-    numberitem = numberitem+1
-    strnumberitem = str(numberitem)+'.'
-    head_bulet = strnumberitem + topic[:x]+endtext
     str = st.radio(head_bulet,['แผนภูมิแท่งแบบต่อกัน'], horizontal=True)
    for topic in list_num_keys:
     numberitem = numberitem+1
