2a2bc4f1c549191f51c0401833ea25cb415817b5
paunpong <154779459+paunpong@users.noreply.github.com>
2024-01-30 13:07:29 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -132,7 +132,7 @@ if upload_file is not None:
     endtext =""
     topic_long = st.radio('แสดงหัวข้อแบบย่อ', ['ใช่', 'ไม่ใช่'], horizontal=True)
     if topic_long =="ใช่":
-      x=20
+      x=25
       endtext = "ฯ"
     numberitem = 0
     
