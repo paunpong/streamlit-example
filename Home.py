d521d1ce550571dd86b88b159027e4c92221a764
paunpong <154779459+paunpong@users.noreply.github.com>
2024-02-02 14:23:55 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -508,7 +508,7 @@ if menu == 'เริ่มต้นโปรแกรม':
    data_num_stack = []
    for pie in list_pie_chart:
     values = count_list(upload_df[pie].values.tolist(), list_pie_chart[pie]['removenan'])
-    color_topic = f'#**{pie}**'
+    color_topic = f'**{#pie}**'
     st.write(color_topic)
     data_pie.append([pie, sum([values[key]['count'] for key in values]), 100])
     for ans in values:
