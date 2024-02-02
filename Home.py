4a49ff68c45fff1ce1d6449ff36c56403ffdc91d
paunpong <154779459+paunpong@users.noreply.github.com>
2024-02-02 16:35:26 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -377,7 +377,7 @@ if menu == 'เริ่มต้นโปรแกรม':
      strnumberitem = str(Number)+')'
      head_bulet = strnumberitem + topic_pie[:x]+endtext
      Pie = st.radio(head_bulet, ['ลบไม่ระบุ','เพิ่มไม่ระบุ'], horizontal=True)
-     list_pie_chart[topic_pie] = {'removenan': True if Pie == 'ลบไม่ระบุ' else False,}
+     list_pie_chart[topic_pie] = {'removenan': True if Pie == 'ลบไม่ระบุ' else False}
      continue
    if Type == 'กล่อง':
     for topic_box in list_boxplot:
@@ -405,7 +405,8 @@ if menu == 'เริ่มต้นโปรแกรม':
      c = Count(upload_df[topic_bar].values.tolist())
      Bar = st.radio(head_bulet, ['ลบไม่ระบุ', 'เพิ่มไม่ระบุ'], horizontal=True)
      #a = st.radio('',[1,3,5,7,max(c.values())], horizontal=True)
-     #z = st.slider('', 1, max(c.values()), 1, 1) 
+     #z = st.slider('', 1, max(c.values()), 1, 1)
+     st.write(c)
      list_bar_chart[topic_bar] = {'removenan': True if Bar == 'ลบไม่ระบุ' else False, 'orther_number': 1}
      continue
    
