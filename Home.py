08543a4c3143af683d8b1f0675a1a2e7df40d6c1
paunpong <154779459+paunpong@users.noreply.github.com>
2024-02-02 14:30:46 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -510,7 +510,7 @@ if menu == 'เริ่มต้นโปรแกรม':
     values = count_list(upload_df[pie].values.tolist(), list_pie_chart[pie]['removenan'])
     color_topic = f'**{pie}**'
     st.write(color_topic)
-    data_pie.append([f'**{pie}**', f'**{sum([values[key]['count'] for key in values]}**'), f'**{100}**'])
+    data_pie.append([f'**{pie}**', sum([values[key]['count'] for key in values]), f'**{100}**'])
     for ans in values:
      count = values[ans]['count']
      percent = values[ans]['percent']
