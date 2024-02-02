7392bec245756317e0869bee05655f5b3497e685
paunpong <154779459+paunpong@users.noreply.github.com>
2024-02-02 17:13:18 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -183,7 +183,7 @@ def bar_chart_new(data,key):
  ax.set_xticks(labels)
  ax.yaxis.set_major_locator(plt.MaxNLocator(integer=True))
  legend = [f'{i + 1}:{data[0][i]}' for i in range(len(data[0]))]
- plt.bar(labels, values, label=labels)
+ plt.bar(labels, values)
  ax.legend(bbox_to_anchor=(1, 0, 0.22, 1), prop=thai_font_prop)
  plt.title(key, fontproperties=thai_font_prop)
  st.pyplot()
