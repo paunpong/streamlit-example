78787664e4ab5005e20c9cfe738a9159a7bc472b
paunpong <154779459+paunpong@users.noreply.github.com>
2024-02-02 14:40:28 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -512,7 +512,7 @@ if menu == 'เริ่มต้นโปรแกรม':
     for ans in values:
      count = values[ans]['count']
      percent = values[ans]['percent']
-     data_pie.append([ans, count, percent])
+     data_pie.append([ans, count, percent],/n)
     data_pie.append(['','',''])
    if list_pie_chart != dict() and {'removenan':True}:
     st.table([head_quality,*data_pie])
