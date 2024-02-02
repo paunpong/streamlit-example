aac209551841a590e0d25d8ee3c775f073d467bc
paunpong <154779459+paunpong@users.noreply.github.com>
2024-02-02 12:30:41 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -179,9 +179,10 @@ def bar_list_count(data,orther_number=1):
 def bar_chart_new(data,key):
  labels = range(1,len(data[0])+1)
  values = data[1]
+ st.write(max(values))
  fig,ax = plt.subplots(figsize=(9,6))
  ax.set_xticks(labels)
- ax.yticks(range(0,max(data[1])))
+ #ax.yticks(range(0,max(data[1])))
  #ax.yaxis.set_major_locator(plt.MaxNLocator(integer=True))
  legend = [f'{i + 1}:{data[0][i]}' for i in range(len(data[0]))]
  plt.bar(labels, values,label=legend)
