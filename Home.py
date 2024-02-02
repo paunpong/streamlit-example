bb2cc573309e3743c81f0322d1b3fe578668fc06
paunpong <154779459+paunpong@users.noreply.github.com>
2024-02-02 16:36:33 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -406,7 +406,7 @@ if menu == 'เริ่มต้นโปรแกรม':
      Bar = st.radio(head_bulet, ['ลบไม่ระบุ', 'เพิ่มไม่ระบุ'], horizontal=True)
      #a = st.radio('',[1,3,5,7,max(c.values())], horizontal=True)
      #z = st.slider('', 1, max(c.values()), 1, 1)
-     st.write(c)
+     st.write(max(c.values()))
      list_bar_chart[topic_bar] = {'removenan': True if Bar == 'ลบไม่ระบุ' else False, 'orther_number': 1}
      continue
    
