9377ff619eb9d4d61fb54f8d28700a1087f1f7cd
paunpong <154779459+paunpong@users.noreply.github.com>
2024-02-04 15:12:13 +0700

    Update t.py

--- t.py
+++ t.py
@@ -182,7 +182,7 @@ def bar_chart_new(data,key):
  ax.yaxis.set_major_locator(plt.MaxNLocator(integer=True))
  for i in range(len(data[0])):
   legend = f'{i + 1}:{data[0][i]}'
-  color = plt.rcParams['axes.prop_cycle'].by_key()['color'][i]
+  color = plt.rcParams['axes.prop_cycle'].by_key()['color']
   ax.bar(labels, values, label=legend, color=color)
  ax.legend(bbox_to_anchor=(1, 0, 0.22, 1),prop=thai_font_prop)
  plt.title(key,fontproperties=thai_font_prop)
