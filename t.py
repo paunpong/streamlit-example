424747b428c2c32a7ad6bdf3ad6e0ec0fd9a4b0b
paunpong <154779459+paunpong@users.noreply.github.com>
2024-02-04 14:57:32 +0700

    Update t.py

--- t.py
+++ t.py
@@ -182,7 +182,7 @@ def bar_chart_new(data,key):
  for i in range(len(data[0])):
   legend = f'{i + 1}:{data[0][i]}'
   ax.bar(labels, values, label=legend, color=plt.rcParams['axes.prop_cycle'].by_key()['color'])
-  ax.legend(color=plt.rcParams['axes.prop_cycle'].by_key()['color'],bbox_to_anchor=(1, 0, 0.22, 1),prop=thai_font_prop)
+ ax.legend(bbox_to_anchor=(1, 0, 0.22, 1),prop=thai_font_prop)
  plt.title(key,fontproperties=thai_font_prop)
  st.pyplot()
  
