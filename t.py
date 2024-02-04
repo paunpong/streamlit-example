5b915e5c804eeaa5618d67296cb41c71eb53fac1
paunpong <154779459+paunpong@users.noreply.github.com>
2024-02-04 14:42:57 +0700

    Update t.py

--- t.py
+++ t.py
@@ -182,9 +182,9 @@ def bar_chart_new(data,key):
  fig, ax = plt.subplots(figsize=(9, 6))
  ax.set_xticks(labels)
  ax.yaxis.set_major_locator(plt.MaxNLocator(integer=True))
- wedges, texts, autotexts = ax.bar(labels, values,color=plt.rcParams['axes.prop_cycle'].by_key()['color'])
- ax.legend(wedges, labels, bbox_to_anchor=(1, 0, 0.22, 1), prop=thai_font_prop)
- #legend = [f'{i + 1}:{data[0][i]}' for i in range(len(data[0]))]
+ legend = [f'{i + 1}:{data[0][i]}' for i in range(len(data[0]))]
+ wedges, texts, autotexts = ax.bar(labels, values, label=legend, color=plt.rcParams['axes.prop_cycle'].by_key()['color'])
+ ax.legend(wedges, bbox_to_anchor=(1, 0, 0.22, 1), prop=thai_font_prop)
  #plt.bar(labels, values, label=legend, color=plt.rcParams['axes.prop_cycle'].by_key()['color'])
  #ax.legend(bbox_to_anchor=(1, 0, 0.22, 1), prop=thai_font_prop)
  plt.title(key, fontproperties=thai_font_prop)
