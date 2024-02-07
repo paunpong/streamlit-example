e0ccec8640b66a70aa2742b2563cd96d543eb3c6
paunpong <154779459+paunpong@users.noreply.github.com>
2024-02-07 12:35:55 +0700

    Update t.py

--- t.py
+++ t.py
@@ -185,7 +185,7 @@ def stacked_bar(data,key):
  name = data.keys()
  data1 = data.values()
 
- st.write(data1)
+
 
  ax.set_yticklabels(name, fontproperties=thai_font_prop)
  d_f = pd.DataFrame(data1,index=name)
