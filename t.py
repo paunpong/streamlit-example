3e077016c9b3e64407b4c89e137f1f5da22fd137
paunpong <154779459+paunpong@users.noreply.github.com>
2024-02-08 21:51:56 +0700

    Update t.py

--- t.py
+++ t.py
@@ -571,12 +571,11 @@ if menu == 'เริ่มต้นโปรแกรม':
     data = upload_df[bar].values.tolist()
     all_data = len(data)
     Val = Count(data,list_bar_chart[bar]['removenan'])
-    st.write(Val)
     other = bar_list_count(Val,list_bar_chart[bar]['orther_number'])
     data_bar.append([bar,'จำนวน', 'เปอร์เซนต์'])
     data_dict = dict(zip(other[0],other[1]))
     for key in data_dict:
-     sum_val = sum([Val[key] for key in Val]
+     sum_val = sum([Val[key] for key in Val])
      st.write(sum_val)
      cou = data_dict[key]
      percent = 100*cou/all_data
