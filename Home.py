805266026455638c86e1b4969767d914fac0af85
paunpong <154779459+paunpong@users.noreply.github.com>
2024-02-14 21:12:32 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -670,8 +670,7 @@ doc = create_word_doc("This is the text content of the document.")
 for p in list_pie_chart:
  pie_chart_path = pie_chart(count_list(upload_df[p].values.tolist(),list_pie_chart[p]['removenan']),p)
  #pie_chart_path = pie_chart(pie_chart_data, pie_chart_key, pie_chart_digit,)
- if list_pie_chart != dict() and {'removenan':True}:
-  doc.add_picture(pie_chart_path)  # เพิ่มรูปภาพ pie chart เข้าไปในเอกสาร
+ doc.add_picture(pie_chart_path)  # เพิ่มรูปภาพ pie chart เข้าไปในเอกสาร
 # Save the document to a BytesIO object
 doc_buffer = io.BytesIO()
 doc.save(doc_buffer)
