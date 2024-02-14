0d7d13ea13b85eece10c0feb6dfe127b938e71bc
paunpong <154779459+paunpong@users.noreply.github.com>
2024-02-14 21:48:55 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -677,16 +677,16 @@ if st.button("Create Word Document"):
  doc = create_word_doc("This is the text content of the document.")
  for p in list_pie_chart:
   pie_chart_path = pie_chart(count_list(upload_df[p].values.tolist(),list_pie_chart[p]['removenan']),p)
-  doc.add_picture(pie_chart_path,width=Inches(3))
+  doc.add_picture(pie_chart_path)
   #pie_chart_path = pie_chart(pie_chart_data, pie_chart_key, pie_chart_digit,)
   #doc.add_picture(io.BytesIO(base64.b64decode(encoded_image)))  # เพิ่มรูปภาพ pie chart เข้าไปในเอกสาร
  # Save the document to a BytesIO object
  
- #doc_buffer = io.BytesIO()
- #doc.save(doc_buffer)
- #doc_buffer.seek(0)
+ doc_buffer = io.BytesIO(pie_chart_path)
+ doc.save(doc_buffer)
+ doc_buffer.seek(0)
  
  # Provide download link for the generated document
- st.download_button(label="Download Word Document",data=doc,file_name="output.docx",
+ st.download_button(label="Download Word Document",data=doc_buffer,file_name="output.docx",
      mime="application/vnd.openxmlformats-officedocument.wordprocessingml.document",key="word-doc-download")
  st.success("Word document created successfully!")
