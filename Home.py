b2e28e4411367ee672b4204744dc3e259fece064
paunpong <154779459+paunpong@users.noreply.github.com>
2024-02-19 16:53:41 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -705,15 +705,16 @@ if menu == 'เริ่มต้นโปรแกรม':
     st.table(data_stack_str)
 
    for strs in list_str_stack:
-    count_string = count_list(upload_df[strs].values.tolist(),list_str_stack[strs]['removenan'])
-    if set(upload_df[Str].values.tolist()).issubset({'มากที่สุด','มาก','ปานกลาง','น้อย','น้อยที่สุด','ไม่ระบุ'}):
-     data_stack_str.append([strs,f"{count_string['มากที่สุด']['count']}({count_string['มากที่สุด']['percent']}%)"if 'มากที่สุด' in count_string else "0(0%)",
+    Col = upload_df[strs].values.tolist()
+    count_string = count_list(Col,list_str_stack[strs]['removenan'])
+    #if set(upload_df[Str].values.tolist()).issubset({'มากที่สุด','มาก','ปานกลาง','น้อย','น้อยที่สุด','ไม่ระบุ'}):
+    data_stack_str.append([strs,f"{count_string['มากที่สุด']['count']}({count_string['มากที่สุด']['percent']}%)"if 'มากที่สุด' in count_string else "0(0%)",
                             f"{count_string['มาก']['count']}({count_string['มาก']['percent']}%)"if 'มาก' in count_string else "0(0%)",
                             f"{count_string['ปานกลาง']['count']}({count_string['ปานกลาง']['percent']}%)"if 'ปานกลาง' in count_string else "0(0%)",
                             f"{count_string['น้อย']['count']}({count_string['น้อย']['percent']}%)"if 'น้อย' in count_string else "0(0%)",
                             f"{count_string['น้อยที่สุด']['count']}({count_string['น้อยที่สุด']['percent']}%)"if 'น้อยที่สุด' in count_string else "0(0%)"])
-    else:
-     data_stack_str.append([strs,f"{count_string[5]['count']}({count_string[5]['percent']}%)"if 5 in count_string else "0(0%)",
+    #else:
+    data_stack_str.append([strs,f"{count_string[5]['count']}({count_string[5]['percent']}%)"if 5 in count_string else "0(0%)",
                             f"{count_string[4]['count']}({count_string[4]['percent']}%)"if 4 in count_string else "0(0%)",
                             f"{count_string[3]['count']}({count_string[3]['percent']}%)"if 3 in count_string else "0(0%)",
                             f"{count_string[2]['count']}({count_string[2]['percent']}%)"if 2 in count_string else "0(0%)",
