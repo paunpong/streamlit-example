8f1024335b6304002ace444c61775397c43bdb5f
paunpong <154779459+paunpong@users.noreply.github.com>
2024-02-20 00:29:53 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -219,7 +219,7 @@ def bar_chart_new(data,key,legend):
   ax.legend(bbox_to_anchor=(1, 0, 0.25, 1),prop=thai_font_prop)
  else:
   Label = data[0]
-  plt.bar(Label,values,prop=thai_font_prop)
+  ax.bar(Label,values,textprops={'fontproperties': thai_font_prop})
  plt.title(key,fontproperties=thai_font_prop)
  chart_bar = f"{key}.png"
  plt.savefig(chart_bar, bbox_inches='tight')
