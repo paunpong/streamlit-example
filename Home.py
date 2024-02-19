498a293147d42460628cd64eb9343bff81fea4c8
paunpong <154779459+paunpong@users.noreply.github.com>
2024-02-20 02:02:04 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -265,7 +265,6 @@ def bar_chart_new(data,key,legend):
    legend = f'{i + 1}:{data[0][i]}'
    ax.bar(labels, values, label=legend,color=color)
    st.pyplot()
-  st.write(f'{range(len(data[0])}:{data[0]}')
  else:
   Label = data[0]
   color=plt.rcParams['axes.prop_cycle'].by_key()['color']
