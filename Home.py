b829fcec84c9d25c3f7af4eca91e8036bc76e56d
paunpong <154779459+paunpong@users.noreply.github.com>
2024-02-21 20:58:59 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -848,9 +848,9 @@ Color = st.radio('ปรับแต่งสีกราฟ', ['ชุดสี
 
 if Color == 'ชุดสีที่ 1':
     plt.rcParams['axes.prop_cycle'] = plt.cycler(color=c)
-elif Color == 'ชุดสีที่ 2':
+if Color == 'ชุดสีที่ 2':
     plt.rcParams['axes.prop_cycle'] = plt.cycler(color=co)
-elif Color == 'ชุดสีที่ 3':
+if Color == 'ชุดสีที่ 3':
     plt.rcParams['axes.prop_cycle'] = plt.cycler(color=cc)
  
 #st.write(plt.rcParams['axes.prop_cycle'])
