2948537438617523a366746f34448158979a750b
paunpong <154779459+paunpong@users.noreply.github.com>
2024-02-21 22:23:38 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -18,7 +18,7 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 from streamlit_option_menu import option_menu   
-
+ 
 #st.set_page_config(page_title="อัปโหลดไฟล์",layout="wide")
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
