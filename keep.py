634dbe33c43049e2c584e56fdd901d76515b8507
paunpong <154779459+paunpong@users.noreply.github.com>
2024-02-22 21:48:53 +0700

    Update keep.py

--- keep.py
+++ keep.py
@@ -139,6 +139,7 @@ def create_word_doc(Pie_chart,Box_chart,Com_bar,Bar_chart,St_str,St_num,Str_st,N
   
  for t_p in table_pie:
   df = create_table(t_p,doc)
+  df.append('\t')
   df.style = 'Table Grid'
  
  for box in Box_chart:
