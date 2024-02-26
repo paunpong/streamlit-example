24f63a1e10344a9d557d6a53d63ff5ec47231eac
paunpong <154779459+paunpong@users.noreply.github.com>
2024-02-26 16:02:41 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -194,7 +194,7 @@ def create_word_doc(Pie_chart,Box_chart,Com_bar,Bar_chart,St_str,St_num,Str_st,N
   df.style = 'Table Grid'# ตารางแท่งต่อกับไม่มีหัวใหญ่ 
 
  for ment in comment:
-  st.write(ment)   
+  st.write(ment[0])   
   doc.add_paragraph(ment[0])
      
  #---------------------------------------------------------------------------ภาพ
