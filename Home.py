7e2b3f6cb0f9f09c7987b72763eb8820c63adede
paunpong <154779459+paunpong@users.noreply.github.com>
2024-02-26 15:38:55 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -206,7 +206,7 @@ def create_word_doc(Pie_chart,Box_chart,Com_bar,Bar_chart,St_str,St_num,Str_st,N
   df.style = 'Table Grid'# ตารางแท่งต่อกับไม่มีหัวใหญ่ 
 
  for COM in comment:
-  df = Table(COM,doc)
+  df = table_comment(COM,doc)
   doc.add_paragraph('\t')
      
  #---------------------------------------------------------------------------ภาพ
