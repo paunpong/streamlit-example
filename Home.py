e0ab76dd87c1e80edea292c1b07326e75db1d2ad
paunpong <154779459+paunpong@users.noreply.github.com>
2024-02-26 19:13:05 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -944,7 +944,7 @@ if menu == 'เริ่มต้นโปรแกรม':
      
     table_str.append(data_stack_str)
     st.table(data_stack_str)
-    data_stack_str = []
+   data_stack_str = []
    #if list_stack_str != dict() and {'removenan':True}:
     #st.table(data_stack_str)
     
