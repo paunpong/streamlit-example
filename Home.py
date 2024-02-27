5e8f2443d93c04f5f69cc06381f749a25421222e
paunpong <154779459+paunpong@users.noreply.github.com>
2024-02-27 12:12:50 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -42,7 +42,7 @@ color_set3 = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', 
 color_set4 = ['#e7ba52', '#e7cb94', '#843c39', '#ad494a', '#d6616b', '#e7969c', '#7b4173', '#a55194', '#ce6dbd', '#de9ed6']
 
 
-upload_file = st.sidebar.file_uploader(" ",type=["csv", "xlsx"])
+
 
 
 
@@ -378,6 +378,7 @@ with st.sidebar:
  #st.markdown('## :red[โปรแกรมสร้างรายงานสรุปจากฟอร์มออนไลน์]')
 
 if menu == 'เริ่มต้นโปรแกรม':
+ upload_file = st.sidebar.file_uploader(" ",type=["csv", "xlsx"])
  #st.markdown('### :rainbow[โปรแกรมสร้างรายงานสรุปจากฟอร์มออนไลน์]')
  st.image("header.png")
  with st.expander('### :red[คำแนะนำ]',expanded=(upload_file is None)):
@@ -394,7 +395,7 @@ if menu == 'เริ่มต้นโปรแกรม':
   st.image("customize.png")
   st.markdown("""---""")
   st.markdown('**:blue[5. เมื่อได้รูปแบบที่ต้องการแล้วกดปุ่มดาวน์โหลดเพื่อโหลดไฟล์ docs]**')
-upload_df = upload(upload_file) 
+ upload_df = upload(upload_file) 
 #-------------------------------------------------แยกหัวข้อ----------------------------------------------------#
 if menu == 'เริ่มต้นโปรแกรม':
  if upload_file is not None:
