47c2e893612048589b612062f4eaeb15b3dbd412
paunpong <154779459+paunpong@users.noreply.github.com>
2024-03-03 23:42:59 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -693,7 +693,7 @@ if upload_file is not None:
       if not form_displayed:
        num = st.radio(i, ['แปลผล', 'ไม่แปลผล'], horizontal=True)
        if num == 'ไม่แปลผล':
-        del list_stack_num[topic_stack]
+        del list_stack_num[n]
        form_displayed = True
    #for topic_stack in list_num_keys:
     #Number = Number+1
