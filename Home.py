50f9afb00e9346783f87bc1d26637a1182d308f6
paunpong <154779459+paunpong@users.noreply.github.com>
2024-03-03 21:53:06 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -1009,7 +1009,8 @@ if upload_file is not None:
      data_str2.append('0(0)')
     else:
      data_str2.append(f'{count_string[i]["count"]}({count_string[i]["percent"]})')
-  st.table(data_str1) 
+   st.table(data_str1)
+  
   if list_str_stack != dict() and {'removenan':True}:
    st.markdown("""---""")
        
