435eeca940dd909e0a530b71ab28e572f5934aa5
paunpong <154779459+paunpong@users.noreply.github.com>
2024-03-03 23:37:45 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -687,11 +687,10 @@ if upload_file is not None:
    if list_stack_num != dict() and {'removenan':True}:
     st.markdown('**:blue[หัวข้อใดที่ต้องการให้แปลผลเป็นระดับความพึงพอใจ (1 น้อยที่สุด – 5 มากที่สุด)  )]**')
    for i in set_topic:
-    num = st.radio(i,['แปลผล','ไม่แปลผล'], horizontal=True)
-    if num == 'ไม่แปลผล':
-     for n in list_num_keys:
-      if i in n:
-       del list_stack_num[topic_stack]
+    if i in list_num_keys:
+     num = st.radio(i,['แปลผล','ไม่แปลผล'], horizontal=True)
+     if num == 'ไม่แปลผล':
+      del list_stack_num[topic_stack]
    #for topic_stack in list_num_keys:
     #Number = Number+1
     #strnumberitem = str(Number)+')'
