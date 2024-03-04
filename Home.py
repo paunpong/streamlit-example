d7d7ca6b40ee5052ad35c3fe088eda1f2040e851
paunpong <154779459+paunpong@users.noreply.github.com>
2024-03-04 08:15:59 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -1002,7 +1002,7 @@ if upload_file is not None:
    st.table(data_stack_str2)
    
   for topic in list_stack_str:
-   topic_word,sub_word = topic.split('[ ')[:2]
+   topic_word,sub_word = topic.split(' [')[:2]
    st.write(topic_word)
   
   data_str3 = list()
