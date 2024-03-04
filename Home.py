e97bbe74f8d81a37b02b83d442172b7ce4394222
paunpong <154779459+paunpong@users.noreply.github.com>
2024-03-04 18:24:04 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -196,7 +196,7 @@ def create_word_doc(Pie_chart,Box_chart,Com_bar,Bar_chart,St_str,St_num,Str_st,N
 
  #doc.add_paragraph(comment)          
  for Ment in comment:
-  doc.add_paragraph(Ment[0][0], style='Heading 1')
+  doc.add_paragraph(Ment[0][0])
   for sub_ment in Ment[1:]:
    doc.add_paragraph(f"- {sub_ment[0]}")
    
