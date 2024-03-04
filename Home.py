56d4b6c965d80f00c3e87e95a11b75a866eef860
paunpong <154779459+paunpong@users.noreply.github.com>
2024-03-04 09:51:53 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -1023,7 +1023,7 @@ if upload_file is not None:
                             f"{count_string['1']['count']}({count_string['1']['percent']}%)"if '1' in count_string else "0(0%)"])
    
    
-   st.table(data_stack_str)
+  st.table(data_stack_str)
    
   
   data_str3 = list()
