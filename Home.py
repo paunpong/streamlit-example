c9c8ce973b805b663bce31a84d5078984f8f7377
paunpong <154779459+paunpong@users.noreply.github.com>
2024-03-04 11:12:16 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -1041,7 +1041,7 @@ if upload_file is not None:
      data_str2.append(f'{count_string[i]["count"]}({count_string[i]["percent"]})')
    data_str1.append(data_str2)
    #data_str3.append(data_str1)
-   st.write(data_str1)
+   #st.write(data_str1)
    st.table(data_str1)
   
   if list_str_stack != dict() and {'removenan':True}:
@@ -1069,7 +1069,7 @@ if upload_file is not None:
 
 if upload_file is not None:
  word_file_path = create_word_doc(Pie_chart,Box_chart,Com_bar,Bar_chart,St_str,St_num,Str_st,Num_st,
-                                  table_pie,table_box,table_comma,table_bar,data_stack_str3,table_num,data_str3,num_table,upload_file,comment)
+                                  table_pie,table_box,table_comma,table_bar,data_stack_str3,table_num,data_str1,num_table,upload_file,comment)
  st.download_button(label="ดาวน์โหลด",data=open(word_file_path, "rb").read(),file_name="report.docx",mime="application/docx")
 
 #st.write(table_pie)
