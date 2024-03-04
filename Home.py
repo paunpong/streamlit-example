de2293cd95716e316fc950e1e6b2c1d13473db31
paunpong <154779459+paunpong@users.noreply.github.com>
2024-03-04 10:00:48 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -1013,9 +1013,9 @@ if upload_file is not None:
    topic_word = topic_word.strip()
    sub_word = sub_word.strip().replace(']', '')
    if topic_word != top_name:
-    data_stack_str.append([topic_word])
     head_table = ['5'+'\nจำนวน(เปอร์เซนต์)','4'+'\nจำนวน(เปอร์เซนต์)','3'+'\nจำนวน(เปอร์เซนต์)','2'+'\nจำนวน(เปอร์เซนต์)','1'+'\nจำนวน(เปอร์เซนต์)']
-    data_stack_str.append(head_table)
+    data_stack_str.append([topic_word,*head_table])
+    #data_stack_str.append(head_table)
     top_name = topic_word
    data_stack_str.append([sub_word,f"{count_string['5']['count']}({count_string['5']['percent']}%)"if '5' in count_string else "0(0%)",
                             f"{count_string['4']['count']}({count_string['4']['percent']}%)"if '4' in count_string else "0(0%)",
