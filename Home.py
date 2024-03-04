1fa13f89d6681285f304d59d2afd98b0041cc4bc
paunpong <154779459+paunpong@users.noreply.github.com>
2024-03-04 11:09:02 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -1041,6 +1041,7 @@ if upload_file is not None:
      data_str2.append(f'{count_string[i]["count"]}({count_string[i]["percent"]})')
    data_str1.append(data_str2)
    data_str3.append(data_str1)
+   st.write(data_str3)
    st.table(data_str1)
   
   if list_str_stack != dict() and {'removenan':True}:
