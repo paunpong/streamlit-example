abf77507a927d4e01e548a1d81586e1f157c5d5b
paunpong <154779459+paunpong@users.noreply.github.com>
2024-03-09 21:23:11 +0700

    Update keep.py

--- keep.py
+++ keep.py
@@ -491,8 +491,8 @@ if upload_file is not None:
 #pie chart แสดงเพิ่มว่า ใส่ ไม่ระบุ หรือไม่
 dict_str_choosen = dict()
 if upload_file is not None:
- def_cl = Classify(upload_df)
- st.write(def_cl[0])
+ list_num_stack,list_str_stack,list_bar_chart_comma,list_boxplot,list_pie_chart,list_bar_chart,list_stack_num,list_stack_str,dic_stackbar_str_choosen = Classify(upload_df)
+ #st.write(def_cl[0])
  list_pie_keys = list(list_pie_chart.keys())
  list_box_keys = list(list_boxplot.keys())
  list_bar_keys = list(list_bar_chart.keys())
