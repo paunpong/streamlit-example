38897353e3b701672aef84e2f6579b859c67e4e9
paunpong <154779459+paunpong@users.noreply.github.com>
2024-03-20 21:52:08 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -334,8 +334,8 @@ def boxplot(data,key,removenan=True,Aver=True):
  st.pyplot()
  return chart_box
 
-def bar_list_count(data,orther_number=2):
- values = [data[key] for key in data if (data[key] > orther_number ) and (key != "ไม่ระบุ")]
+def bar_list_count(data,orther_number=1):
+ values = [data[key] for key in data if (data[key] >= orther_number ) and (key != "ไม่ระบุ")]
  st.write(values)
  values_orther =  [data[key] for key in data if (data[key] <= orther_number ) and (key != "ไม่ระบุ")]
  labels = [key for key in data if (data[key] > orther_number ) and (key != "ไม่ระบุ")]
